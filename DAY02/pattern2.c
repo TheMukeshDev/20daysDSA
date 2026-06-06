0dd486c61f02b07c1e9a15248986ba01be98f59a
themukeshdev <mukeshkumar916241@gmail.com>
2026-06-06 12:13:52 +0530

    added new  code of pattern

---
+++ DAY02/pattern2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    // *
+    // **
+    // ***
+    // ****
+    int num;
+    printf("enter the number");
+    scanf("%d",&num);
+    for (int i = 1; i <= num; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+}
