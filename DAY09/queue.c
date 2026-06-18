3a6960005350ef77023e5ce6a1d9e7d995d01875
themukeshdev <mukeshkumar916241@gmail.com>
2026-06-18 11:45:34 +0530

    added queue

--- DAY09/queue.c
+++ DAY09/queue.c
@@ -1,103 +1,83 @@
 #include <stdio.h>
-
 #define size 6
 
-int queue[size];
-int front = -1;
-int rear = -1;
-
+int queue[size-1];
+int rear=-1;
+int front=-1;
 void enqueue()
 {
-    if (rear == size - 1)
+    if (rear>size-1)
     {
-        printf("Queue Overflow\n");
+        printf("queue overflow ");
     }
     else
     {
         int d;
-        printf("Enter element: ");
-        scanf("%d", &d);
-
-        if (front == -1)
-        {
-            front = 0;
-        }
-
-        rear++;
-        queue[rear] = d;
+        printf("enter the elements: ");
+        scanf("%d",&d);
+        rear=rear+1;
+        queue[rear]=d;
     }
-}
+    if (front==-1)
+    {
+        front=0;
+    }
 
+
+}
 void dequeue()
 {
-    if (front == -1 || front > rear)
+    if (front==-1 || front>rear)
     {
-        printf("Queue Underflow\n");
+        printf("stack is underflow");
     }
     else
     {
-        printf("%d element is deleted\n", queue[front]);
-        front++;
-
-        if (front > rear)
-        {
-            front = rear = -1;
-        }
+        printf("%d element is deleted ",queue[front]);
+        front=front+1;
     }
+
 }
 
 void display()
 {
-    if (front == -1)
+    int i;
+    if (front==-1 ||front>rear)
     {
-        printf("Queue is empty\n");
+        printf("stack is underflow");
     }
     else
     {
-        printf("Queue elements are:\n");
-
-        for (int i = front; i <= rear; i++)
+        for (int i = front ;  i <= rear ; i++)
         {
-            printf("%d ", queue[i]);
+            printf("%d  ",queue[i]);
         }
-
-        printf("\n");
     }
 }
 
-int main()
-{
+int main(){
     int c;
-
-    while (1)
+    while (true)
     {
-        printf("\n1-Enqueue");
-        printf("\n2-Dequeue");
-        printf("\n3-Display");
-        printf("\n4-Exit");
-        printf("\nEnter choice: ");
-
-        scanf("%d", &c);
-
+        
+        printf("\n1-for Enqueue\n2-for Dequeue\n3-for display\n4-for exit\nEnter your Choise: ");
+        scanf("%d",&c);
         switch (c)
         {
         case 1:
             enqueue();
             break;
-
         case 2:
             dequeue();
             break;
-
         case 3:
             display();
-            break;
-
+            break;  
         case 4:
             return 0;
-
         default:
-            printf("Invalid Input\n");
+            printf("inavlid input");
+            // break;
         }
     }
-}
+} 
